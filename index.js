16d9a2215e36f1fe4455b2269f2b50d3ed751bc9
yannick-98 <yannick9862@gmail.com>
2024-02-21 16:52:29 +0100

    upload files route

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "kalyapp.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type" + "Authorization");
   next();
 });
 
